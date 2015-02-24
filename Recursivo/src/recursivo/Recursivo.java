f94d3dbadac28b91f064b718777f3bc16b88f26f
Julian Gawron <julian.gawron@gmail.com>
2015-02-24 13:18:15 +0100

    d

--- Recursivo/src/recursivo/Recursivo.java
+++ Recursivo/src/recursivo/Recursivo.java
@@ -1,12 +1,29 @@
 package recursivo;
 
+import java.util.Scanner;
+
 /**
  *
- * Recursividad, hay que establecer el caso base que funciona como salida
- * y el caso recursivo, que volverá a llamrer
+ * Sea factorial (n) el método que calcula el factorial de un número n. 
+ * Si llamamos al método con n=0, inmediatamente devuelve el resultado. 
+ * El método conoce cómo resolver el caso más simple (0! = 1), el cual es 
+ * conocido como caso base. Si llamamos al método con n>0, el método reduce 
+ * el problema a la resolución de un subproblema más simple, (n-1)!. 
+ * El subproblema es esencialmente el mismo que el problema original, a 
+ * excepción de que es más simple o más "pequeño". Como el subproblema tiene 
+ * las mismas propiedades que el problema original, podemos invocar al método 
+ * con argumentos diferentes (diferentes en valor, no en número y/o tipo). 
+ * A esto se le conoce como llamada recursiva.
  */
 public class Recursivo {
 
+    public static int pedirDatos(){
+        Scanner entrada = new Scanner(System.in);
+                
+          System.out.println("Itroduzca un numero mayor que 0:");
+       
+            return entrada.nextInt();
+    }
     public static int factorial(int n){
            System.out.print(n + " * ");
             if(n == 0){//caso base
@@ -16,7 +33,7 @@ public class Recursivo {
             }
     }
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        System.out.println(factorial(pedirDatos()));
     }
     
 }
