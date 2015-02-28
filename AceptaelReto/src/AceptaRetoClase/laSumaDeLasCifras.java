b41b5b564af083193b6d148a100f3a4273db8196
Julian Gawron <julian.gawron@gmail.com>
2015-02-28 17:25:23 +0100

    laSumaDeCifrasTerminado
    Arreglado, por que el long no aguanta, y lo hice concatenando Strings

--- AceptaelReto/src/AceptaRetoClase/laSumaDeLasCifras.java
+++ AceptaelReto/src/AceptaRetoClase/laSumaDeLasCifras.java
@@ -16,9 +16,39 @@ public class laSumaDeLasCifras {
         Scanner sc = new Scanner(System.in);
         int dato = sc.nextInt();
         while( dato > 0 ){
-
             
-        System.out.println();
+           int resto = dato % 9;
+           dato = dato - resto;
+           int cuantos9 = dato/9;
+           String respuesta = "";
+
+    	if(dato > 0){
+    		int i;
+    		for (i = 0; i < cuantos9; ++i) {
+    			respuesta += "9";
+                }
+    	}
+        
+        if(resto>0){
+            respuesta = resto + respuesta;
+        }
+        
+        System.out.println(respuesta);
+            
+          /*  int total = 0;
+            int unidad = 1;
+            while(dato > 0){
+                
+                if((dato) <= 9 ){
+                    total = total + (dato * unidad);
+                }
+                else{
+                    total = total + ( unidad * 9);
+                }
+                dato -= 9;
+                unidad *= 10;
+            }
+            System.out.println(total);*/
         dato = sc.nextInt();
         }//sale del programa sin hacer nada.        
     }
