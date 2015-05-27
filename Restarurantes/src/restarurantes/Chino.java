3d5d22f45390d2eb84dcdd5699a967c47444a63a
Juliancito <inutilvandalo@gmail.com>
2015-05-27 12:49:51 +0200

    Cambios en Restaurantes

--- Restarurantes/src/restarurantes/Chino.java
+++ Restarurantes/src/restarurantes/Chino.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package restarurantes;
 
 import java.util.ArrayList;
@@ -43,8 +38,10 @@ public class Chino extends Restarurante{
 
      @Override
     public String mostrarInformacion() {
-        return "Chino{" + "trabajadoresChino=" + trabajadoresChino + '}';
-    }
+    //llamada a super??
+        return super.mostrarInformacion() + 
+                "\n y es un Chino";
+    } 
    
 
 }
--- Restarurantes/src/restarurantes/Restarurantes.java
+++ Restarurantes/src/restarurantes/Restarurantes.java
@@ -1,5 +1,7 @@
 package restarurantes;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Usuario
@@ -12,14 +14,23 @@ public class Restarurantes {
     public static void main(String[] args) {
         Restarurante Luiggi = new Pizzeria("Luiggi", "Pepe", "Santa EnGracia 12");
         Pizzeria MammaLucia = new Pizzeria("MammaLucia", "Cacho", "Rio 13");
+        Chino Bruce_Lee = new Chino("Bruce Lee", "Chung Lee", "Rio 13");
         Restarurante Napoles = new Pizzeria();
         Restarurante Rialto = new Pizzeria();
         Restarurante Milano = new Pizzeria();
         Restarurante OhSoleMio = new Pizzeria();
         System.out.println("Total pizzerias: " + Pizzeria.getTotalPizzerias());
         
-        Trabajador Marcelo = new Trabajador("Marcelo", 30, "SiempreViva 15");
-        System.out.println(MammaLucia.mostrarInformacion());
+        ArrayList<Restarurante> Restaurantes = new ArrayList();
+        Restaurantes.add(MammaLucia);
+        Restaurantes.add(Luiggi);
+        Restaurantes.add(Bruce_Lee);
+        //Recorremos el arrayList Restaurantes probando Polimorfismo
+        for(Restarurante miRestaurant : Restaurantes){
+            System.out.println(miRestaurant.mostrarInformacion());
+        }
+        
+ 
         MammaLucia.darAltaTrabajador(MammaLucia.getTrabajadoresPizzeria());
         MammaLucia.darAltaTrabajador(MammaLucia.getTrabajadoresPizzeria());
         MammaLucia.darAltaTrabajador(MammaLucia.getTrabajadoresPizzeria());
@@ -36,5 +47,6 @@ public class Restarurantes {
             System.out.println(trabajador.getNombre() + " " + trabajador.getEdad());
         }
         
+
     }    
 }
