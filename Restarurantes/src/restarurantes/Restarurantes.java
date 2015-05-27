db4ccd4332902b9e2b06515477dada8835cbbc2c
Juliancito <inutilvandalo@gmail.com>
2015-05-27 09:34:31 +0200

    Utilizando Restaurantes con downcasting

--- Restarurantes/src/restarurantes/Restarurantes.java
+++ Restarurantes/src/restarurantes/Restarurantes.java
@@ -11,8 +11,21 @@ public class Restarurantes {
      */
     public static void main(String[] args) {
         Restarurante Luiggi = new Pizzeria("Luiggi", "Pepe", "Santa EnGracia 12");
-        Restarurante MammaLucia = new Pizzeria("MammaLucia", "Cacho", "Rio 13");
-        System.out.println(Pizzeria.getTotalPizzerias());
-       // MammaLucia.meterTrabajador(getTrabajadoresPizzeria());
+        Pizzeria MammaLucia = new Pizzeria("MammaLucia", "Cacho", "Rio 13");
+        Restarurante Napoles = new Pizzeria();
+        Restarurante Rialto = new Pizzeria();
+        Restarurante Milano = new Pizzeria();
+        Restarurante OhSoleMio = new Pizzeria();
+        System.out.println("Total pizzerias: " + Pizzeria.getTotalPizzerias());
+        
+        Trabajador Marcelo = new Trabajador("Marcelo", 30, "SiempreViva 15");
+        MammaLucia.darAltaTrabajador(MammaLucia.getTrabajadoresPizzeria());
+        
+        //Probando el downcasting
+        if(Luiggi instanceof Pizzeria){
+         Luiggi.darAltaTrabajador(((Pizzeria)Luiggi).getTrabajadoresPizzeria());
+        }
+       
+        
     }    
 }
